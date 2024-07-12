c7ecb713e247cb9a0ddfa648608c16a5f5514565
Angel Paul Patiño Diaz <146494692+Anyel-top@users.noreply.github.com>
2024-07-12 11:45:10 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require('node:http');
+let app = require('express')(); // manager function 
+const http = require('node:http').Server(app);
+const express = require('express'); // manager data
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+/* manager responses 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
-});
+});*/
 
-server.listen(port, hostname, () => {
+// route 
+app.use(require('./routes/cliente'));
+
+app.use(express.json)
+http.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- routes/cliente.js
+++ routes/cliente.js
@@ -3,3 +3,25 @@ const express = require('express');
 const router = express.Router();
 
 var getConecction = require('../conexion');
+
+
+router.get('/usuario/:cedula', (req, res) => {
+    getConecction(function(error, conn) {
+        if (error) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        const { cedula } = req.params;
+        conn.query('SELECT * FROM cliente WHERE cedula = ?', [cedula], function(error, rows) {
+            conn.release();
+            if (error) {
+                res.status(400).send('No se ha podido obtener los datos');
+                return;
+            }
+            res.send(rows);
+        });
+    });
+});
+
+module.exports = router;
